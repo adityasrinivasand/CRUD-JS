6ee0e3dddd6933589624296e07c7ea25603fc83a
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-07-22 09:42:16 +0530

    Add files via upload

--- source/script.js
+++ source/script.js
@@ -129,4 +129,54 @@ function login() {
         window.location.assign("../source/home.html");
     }
 
+<<<<<<< HEAD
 }
+
+function search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myinput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ptable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+  
+/*
+ var table = new Tabulator("#ptable", {
+	data:tabledata,           //load row data from array
+	layout:"fitColumns",      //fit columns to width of table
+	responsiveLayout:"hide",  //hide columns that dont fit on the table
+	tooltips:true,            //show tool tips on cells
+	addRowPos:"top",          //when adding a new row, add it to the top of the table
+	history:true,             //allow undo and redo actions on the table
+	pagination:"local",       //paginate the data
+	paginationSize:7,         //allow 7 rows per page of data
+	movableColumns:true,      //allow column order to be changed
+	resizableRows:true,       //allow row order to be changed
+	initialSort:[             //set the initial sort order of the data
+		{column:"pname", dir:"asc"},
+	],
+	columns:[                 //define the table columns
+		{title:"id", field:"id", editor:"input"},
+		{title:"pname", field:"pname", align:"left", formatter:"progress", editor:true},
+		{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
+		{title:"Rating", field:"rating", formatter:"star", align:"center", width:100, editor:true},
+		{title:"Color", field:"col", width:130, editor:"input"},
+		{title:"Date Of Birth", field:"dob", width:130, sorter:"date", align:"center"},
+		{title:"Driver", field:"car", width:90,  align:"center", formatter:"tickCross", sorter:"boolean", editor:true},
+	],
+}); */
+
+=======
+}
+>>>>>>> ebdd0913995b2f806e610af193b3c0509ba2ad5a
